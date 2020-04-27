0df635dc295e3af7d77e76f0bb97e42a606f64f1
GauravAB <bhokare.gaurav@gmail.com>
2020-04-28 01:19:57 +0530

    sphere added

--- Test/main.cu
+++ Test/main.cu
@@ -19,13 +19,30 @@ void check_cuda(cudaError_t result, char const* const func, const char* const fi
 	}
 }
 
+__device__ bool hit_sphere(const vec3& center, float radius, const ray& r)
+{
+	vec3 oc = r.origin() - center;
+
+	float a = dot(r.direction(), r.direction());
+	float b = 2.0f * dot(oc, r.direction());
+	float c = dot(oc, oc) - radius * radius;
+	float discriminant = b * b - 4.0f * a * c;
+
+	return (discriminant > 0.0f);
+}
+
+
 __device__ vec3 color(const ray& r)
 {
-	vec3 unit_direction = unit_vector(r.direction());
-	float  t = 0.5f * (unit_direction.y() + 1.0f);
-	
+	if (hit_sphere(vec3(0, 0, -1), 0.5, r))
+	{
+		return vec3(1, 0, 0);
+	}
 
-	return (1.0f - t) * vec3(1.0, 1.0, 1.0) + t * vec3(0.5, 0.7, 1.0);
+	vec3 unit_direction = unit_vector(r.direction());
+
+	float  t = 0.5f * (unit_direction.y() + 1.0f);
+	return (1.0f - t) * vec3(1.0f, 1.0f, 1.0f) + t * vec3(0.5f, 0.7f,1.0f);
 }
 
 __global__ void render(vec3* fb, int max_x, int max_y, vec3 lower_left_corner, vec3 horizontal, vec3 vertical, vec3 origin)
