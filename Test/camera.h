3bb80995ebd7d10d7a3a499ca1e95fa917b969e3
GauravAB <bhokare.gaurav@gmail.com>
2020-04-28 17:23:35 +0530

    added better camera

--- Test/camera.h
+++ Test/camera.h
@@ -1,6 +1,6 @@
 #pragma once
 #include "ray.h"
-
+#define M_PI 3.14159265
 
 class camera
 {
@@ -10,17 +10,25 @@ public:
 	vec3 horizontal;
 	vec3 vertical;
 
-	__device__ camera() 
+	__device__ camera(vec3 lookfrom, vec3 lookat, vec3 vup, float vfov, float aspect) 
 	{
-		lower_left_corner = vec3(-2.0, -1.0, -1.0);
-		horizontal = vec3(4.0, 0.0, 0.0);
-		vertical = vec3(0.0, 2.0, 0.0);
-		origin = vec3(0.0, 0.0, 0.0);
+		vec3 u, v, w;
+		float theta = vfov * M_PI / 180;
+		float half_height = tan(theta / 2);
+		float half_width = half_height * aspect;
+		origin = lookfrom;
+		//camera basis orthonormal axis
+		w = unit_vector(lookfrom - lookat);
+		u = unit_vector(cross(vup,w));
+		v = cross(w, u);
+		lower_left_corner = origin - half_width * u - half_height * v - w;
+		horizontal = 2 * half_width * u;
+		vertical = 2 * half_height * v;
 	}
 	
-	__device__ ray get_ray(float u , float v)
+	__device__ ray get_ray(float s , float t)
 	{
-		return ray(origin, lower_left_corner + u * horizontal + v * vertical - origin);
+		return ray(origin, lower_left_corner + s * horizontal + t * vertical - origin);
 	}
 };
 
